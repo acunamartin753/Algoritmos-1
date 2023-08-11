2d44478793f7e48ddd2284f99d44acd9752a1906
acunamartin753 <142049778+acunamartin753@users.noreply.github.com>
2023-08-11 16:52:32 -0300

    Add files via upload

---
+++ algoI_tp2-master/cmake-build-debug/CMakeFiles/tpi.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/Funciones_TPI.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/Funciones_TPI.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/auxiliares.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/auxiliares.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/ejercicios.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/ejercicios.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/main.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/main.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/corregirRegionTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/corregirRegionTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/costoSubsidioMejoraTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/costoSubsidioMejoraTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/creceElTeleworkingEnCiudadesGrandesTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/creceElTeleworkingEnCiudadesGrandesTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/esEncuestaValidaTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/esEncuestaValidaTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/generarJoinTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/generarJoinTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/histHabitacionalTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/histHabitacionalTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/histogramaDeAnillosConcentricosTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/histogramaDeAnillosConcentricosTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/laCasaEstaQuedandoChicaTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/laCasaEstaQuedandoChicaTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/muestraHomogeneaTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/muestraHomogeneaTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/ordenarRegionYCODUSUTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/ordenarRegionYCODUSUTEST.o"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/test/quitarIndividuosTEST.cpp" "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/CMakeFiles/tpi.dir/test/quitarIndividuosTEST.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/googletest-master/googletest/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/Users/martinacuna/Documents/Facultad/Algoritmos/TPI/algoI_tp2/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
